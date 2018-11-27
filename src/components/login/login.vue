ae7b467b6ec2c5db26a291d41099411aff868669
david <hx34561723@163.com>
2018-11-27 15:42:00 +0800

    登陆组件初步

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,15 +1,47 @@
 <template>
-    <div>
-        登陆组件
-    </div>
+  <div class="login-wrap">
+    <el-form label-position="top" label-width="80px" :model="formdata" class="login-form">
+        <h2>用户登陆</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+        <el-button type="success" plain class="login-btn">登陆</el-button>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data(){
+      return{
+          formdata:{
+              username:'',
+              password:'',
+          }
+      }
+  }
 }
 </script>
 
 <style>
-
+.login-wrap{
+    height: 100%;
+    background-color:#324152;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.login-wrap .login-form{
+   width: 400px;
+   background-color:#fff;
+   border-radius: 15px;
+   padding: 30px;
+}
+.login-wrap .login-btn{
+        width:100%;
+        background-color: skyblue;
+}
 </style>
